91ac790960eb507be295184fafae509d18dc79e2
LuanRamalho <32373552+LuanRamalho@users.noreply.github.com>
2021-02-18 14:05:16 -0300

    Add files via upload

---
+++ DrawSmileyTeste.java
@@ -0,0 +1,19 @@
+package Sorriso;
+import javax.swing.JFrame;
+
+public class DrawSmileyTeste
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        DrawSmiley panel = new DrawSmiley();
+        JFrame application = new JFrame();
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.add(panel);
+        application.setSize(230, 250);
+        application.setVisible(true);
+    }
+    
+}
